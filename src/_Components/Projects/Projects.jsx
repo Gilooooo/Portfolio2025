c4a80f86a0416edfe374df6420c072c356e98749
Gilooooo <118784503+Gilooooo@users.noreply.github.com>
2025-08-27 12:32:58 +0800

    Adding Project

--- src/_Components/Projects/Projects.jsx
+++ src/_Components/Projects/Projects.jsx
@@ -5,14 +5,16 @@ export default function Projects() {
   const [appear, setAppear] = useState("");
   const [index, setIndex] = useState(null);
 
-  const Projects = ["Marci Metzger Recreate", "Ojt Tracking System", "Recipe Book with AI"];
+  const Projects = ["Marci Metzger Recreate", "Ojt Tracking System", "Recipe Book with AI", "Capstone Autochecker Project"];
   const ProjectQuote = [
     "A complete redesign of the Marci Metzger website. I recreated the entire UI with a fresh, modern design approach—focusing on clean layouts, professional aesthetics, and improved user experience.",
+    "Checking Made easy with OCR"
     "Currently this is under construction",
     "Currently this is under construction",
   ];
   const ProjectUrl = [
     "https://marci-metzger-sooty.vercel.app",
+    "https://capstone-auto-checker.vercel.app",
     "Under Construction",
     "Under Construction",
   ];
